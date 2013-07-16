c0fcc92c73483b7788d7236642b585f4cb27acbd
Dave Evans <devans725@hotmail.com>
2013-07-16 12:14:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EbmStatic::Application.config.secret_key_base = '3719bbdb176a669021a0e7e57e22f7589b7dbe363ccc99a0481d3d1359d6fa248562d095e852cf61a71e75b513dd3c7803a0e9cbfc5c08c2c405b94b8b6a81ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EbmStatic::Application.config.session_store :cookie_store, key: '_ebm_static_session'
